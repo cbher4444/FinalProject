1dcd01198d36461614b87c4de114291f597bad57
entance1 <dlxor968@gmail.com>
2024-10-07 17:31:44 +0900

    1대1챗 구현중

--- final/src/main/java/com/kh/usTwo/home/controller/HomeController.java
+++ final/src/main/java/com/kh/usTwo/home/controller/HomeController.java
@@ -5,16 +5,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.usTwo.member.model.vo.Member;
+
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
 	public String home(HttpSession session) {
 		
-		String loginUser = null;
+		Member loginUser = null;
 		
 		if(session.getAttribute("loginUser") != null) {
-			loginUser = (String)session.getAttribute("loginUser");
+			loginUser = (Member)session.getAttribute("loginUser");
 		}
 		
 		if(loginUser == null) {
